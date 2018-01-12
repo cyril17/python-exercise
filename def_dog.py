c6969b3a282b99120464509b916b2f0de16474ee
cyril <36559067@qq.com>
2018-01-12 11:04:15 +0800

    修改默认值

--- def_dog.py
+++ def_dog.py
@@ -1,4 +1,7 @@
 def describe(pet_name,animal_type='dog'):
     print("I have a " + animal_type.title() + ".")
     print("my " + animal_type + "'name is " + pet_name.title() + "!")
+
+
+print()
 describe()
