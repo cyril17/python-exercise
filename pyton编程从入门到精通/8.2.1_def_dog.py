15a02ace840bb5089689511ed84661b0807f5552
cyril <36559067@qq.com>
2018-01-12 11:55:13 +0800

    给形参设置默认值

--- pyton编程从入门到精通/8.2.1_def_dog.py
+++ pyton编程从入门到精通/8.2.1_def_dog.py
@@ -2,6 +2,4 @@ def describe(pet_name,animal_type='dog'):
     print("I have a " + animal_type.title() + ".")
     print("my " + animal_type + "'name is " + pet_name.title() + "!")
 
-
-print()
 describe()
