ec3773b5fe0952f57023b429a731fcf68463a70c
cyril <36559067@qq.com>
2018-01-12 17:01:47 +0800

    返回字典

--- pyton编程从入门到精通/8.3.3_person.py
+++ pyton编程从入门到精通/8.3.3_person.py
@@ -3,3 +3,13 @@ def build_person(first_name,last_name):
     return person
 musician = build_person('cyril','guo')
 print(musician)
+
+
+def build_person(first_name,last_name,age=''):
+    person = {'first':first_name,'last':last_name}
+    if age:
+        person['age'] = age
+    return person
+
+musician2 = build_person('cyril','guo','26')
+print(musician2)
