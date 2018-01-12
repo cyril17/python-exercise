d2b228c7f3e6a79da72c5956368a1f75b08aaaf4
cyril <36559067@qq.com>
2018-01-12 16:23:50 +0800

    非空字符串定义为true return的作用：结束一个函数的执行

--- pyton编程从入门到精通/8.3.2_or_get_formatted_name.py
+++ pyton编程从入门到精通/8.3.2_or_get_formatted_name.py
@@ -11,7 +11,7 @@ def get_formatted_name(first_name,last_name,middle_name=''):
         full_name = first_name + ' '+ middle_name +' '+ last_name
     else:
         full_name = first_name + ' ' + last_name
-    return full_name.title()
+    return full_name.title()  #return的作用：结束一个函数的执行
 
 musician = get_formatted_name('cyril','guo')
 
