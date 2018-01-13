4594ae09081cad993dabd66f7f43633fa8d85840
cyril <36559067@qq.com>
2018-01-13 23:15:39 +0800

    from module_name import function_name 导入特定的函数

---
+++ pyton编程从入门到精通/8.6.2_making_pizza.py
@@ -0,0 +1,15 @@
+from pizza import make_pizza
+
+make_pizza(16,'popperoni')
+make_pizza(12,'mushrooms','green peppers','extra cheese')
+
+'''
+运行结果
+Making 16's pizza with following topping:
+- popperoni
+
+Making 12's pizza with following topping:
+- mushrooms
+- green peppers
+- extra cheese
+'''
