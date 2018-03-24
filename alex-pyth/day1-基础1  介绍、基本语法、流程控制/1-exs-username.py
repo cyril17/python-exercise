2c3b6c7babc7a96a71180520f68b3714b92bccfb
cyril <36559067@qq.com>
2018-03-24 18:50:43 +0800

    .

--- alex-pyth/day1-基础1  介绍、基本语法、流程控制/1-exs-username.py
+++ alex-pyth/day1-基础1  介绍、基本语法、流程控制/1-exs-username.py
@@ -10,16 +10,24 @@ _password='111'
     print("you got it!")
 else:
 """
+d = {}
+error = {}
 cout = 0
 while cout <3:
 
-    usename = input("please input username:")
+    username = input("please input username:")
     password = input("input password:")
-
-    if _username==usename and _password==password:
-        print("you got it")
+    if username == error.keys():
+        print("you can't login!")
         break
     else:
-        print("sorry,input error,please input again:")
-    cout +=1
+        if _username==username and _password==password:
+            print("you got it",d)
+            d[username] = password
+            break
+        else:
+            print("sorry,input error,please input again:")
+        cout +=1
+
+error[username] = password
 print("you have input error more times,so closed it!")
