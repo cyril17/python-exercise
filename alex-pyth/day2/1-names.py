aceae4e38dffaace5feeccaca4ed26935faa240a
cyril <36559067@qq.com>
2018-06-20 07:08:44 +0800

    .

--- alex-pyth/day2/1-names.py
+++ alex-pyth/day2/1-names.py
@@ -1,4 +1,34 @@
 
-names = ['gsy','txy','tdb']
+names = ['gsy','txy','tdb','wq','re','re','tt']
 
 print(names[0],names[2])
+print(names[0::3])
+
+names.append('我是no')
+print(names)
+
+names.insert(1,'bb')
+print(names)
+
+names[3] = 'go'
+print(names)
+
+del names[3]
+print(names)
+
+names.remove('tt')
+print(names)
+
+names.pop()
+print('pop-----',names)
+
+names2 = [1,2,3]
+names.extend(names2)
+print('extend-----',names)
+
+#names.clear()
+#print(names)
+
+names3 = names.copy()
+print(names3)names[1]=666
+print(names,names3)
