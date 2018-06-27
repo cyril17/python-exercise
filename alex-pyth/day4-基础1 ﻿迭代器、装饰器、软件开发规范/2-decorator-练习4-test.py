a0757e977e691efce80f5b66889506465be053dc
cyril <36559067@qq.com>
2018-06-27 20:37:33 +0800

    装饰器

---
+++ alex-pyth/day4-基础1 ﻿迭代器、装饰器、软件开发规范/2-decorator-练习4-test.py
@@ -0,0 +1,53 @@
+
+user,passwd = 'gsy','123'
+
+def auth(auth_type):
+    print('auth_type:',auth_type)
+    def outwrapper(func):
+        def wrapper(*args,**kwargs):
+            print('wrapper func args:',*args,**kwargs)
+            if auth_type == 'local':
+                username = input('Username:').strip()
+                password = input('Password:').strip()
+
+                if user == username and passwd == password:
+                    print('\033[32;1m Authentication has passed\033[0m')
+                    res = func(*args,**kwargs)
+                    print('------------after authentication')
+                    return res
+                else:
+                    exit('Invalid username or password')
+            elif auth_type == 'ldap':
+                print('不会')
+        return wrapper
+    return outwrapper
+
+def index():
+    print('welcome to index page')
+
+@auth(auth_type='local')
+def home():
+    print('welcome to home page')
+    return 'from home'
+
+@auth(auth_type='ldap')
+def bbs():
+    print('welcome to bbs page')
+
+index()
+print(home())
+bbs()
+'''
+auth_type: local
+auth_type: ldap
+welcome to index page
+wrapper func args:
+Username:gsy
+Password:123
+ Authentication has passed
+welcome to home page
+------------after authentication
+from home
+wrapper func args:
+不会
+'''
