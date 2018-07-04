045452918f65a696a82e723451c5a2662626618d
cyril <36559067@qq.com>
2018-07-05 00:39:26 +0800

    .

--- alex-pyth/day5-modules/shutil_test/shutil模块.py
+++ alex-pyth/day5-modules/shutil_test/shutil模块.py
@@ -1,4 +1,14 @@
+# -*- coding: utf-8 -*-
 
+import shutil
+
+f1 = open('qw1',encoding='utf-8')
+
+f2 = open('qw2','w',encoding='utf-8')
+
+shutil.copyfileobj(f1,f2)
+
+#将qw1的内容拷贝到qw2中去
 
 
 shutil.copyfile('qw1','qw3')
