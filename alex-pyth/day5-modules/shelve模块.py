44bd1d4e72152f3ebbc1d8459c0cab6706d3d48c
cyril <36559067@qq.com>
2018-07-07 15:35:59 +0800

    .

---
+++ alex-pyth/day5-modules/shelve模块.py
@@ -0,0 +1,27 @@
+
+import shelve
+import datetime
+
+d = shelve.open('shelve_test')
+
+# info = {'age':22,'job':'it'}
+#
+# name = ['gsy','rain','sun']
+#
+# d['name'] = name   #持久dict
+# d['info'] = info   #持久列表
+# d['date'] = datetime.datetime.now()
+#
+# d.close()
+
+print(d.get('name'))
+print(d.get('info'))
+print(d.get('date'))
+
+'''
+['gsy', 'rain', 'sun']
+{'age': 22, 'job': 'it'}
+2018-07-05 11:01:22.940575
+'''
+
+
